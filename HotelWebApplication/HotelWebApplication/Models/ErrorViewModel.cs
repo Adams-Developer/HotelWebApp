e8c81ae690faef900b841095f81834c8cdd10524
Tiffany Adams <tadams03@mail.roosevelt.edu>
2020-09-05 09:30:58 -0500

    Added new net core project with individual user account authentication

---
+++ HotelWebApplication/HotelWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HotelWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
